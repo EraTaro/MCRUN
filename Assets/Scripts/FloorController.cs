9412cf539d5692ed57f5dcafd386223e9d7531a2
Taro Era <era@crypton.co.jp>
2019-10-12 16:58:23 +0900

    initial commit. 動く床の無限ループ

---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour
+{
+    public enum State
+    {
+        IDLE,
+        MOVE
+    }
+
+    private GameObject obj;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private State state = State.IDLE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = Instantiate((GameObject)Resources.Load("Floor"), new Vector3(transform.position.x, transform.position.y, transform.position.z - ((GameObject)Resources.Load("Floor")).GetComponent<Renderer>().bounds.extents.z), Quaternion.identity);
+        obj.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obj.transform.position.z <= transform.position.z - obj.GetComponent<Renderer>().bounds.extents.z)
+        {
+            obj = Instantiate((GameObject)Resources.Load("Floor"), new Vector3(transform.position.x, transform.position.y, obj.transform.position.z + obj.GetComponent<Renderer>().bounds.size.z - speed * Time.deltaTime), Quaternion.identity);
+            obj.transform.parent = transform;
+        }
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+    public State getState()
+    {
+        return state;
+    }
+}
---
+++ Assets/Scripts/FloorMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorMover : MonoBehaviour
+{
+    [SerializeField] float speed =10.0f ;
+    private float initPositionZ = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPositionZ = transform.localPosition.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.parent.GetComponent<FloorController>().getState().Equals(FloorController.State.IDLE))
+        {
+            return;
+        }
+
+        Debug.Log("world position is " + transform.position);
+        Debug.Log("local position is " + transform.localPosition);
+        float speed = transform.root.gameObject.GetComponent<FloorController>().getSpeed();
+        this.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed * Time.deltaTime);
+
+        if (transform.localPosition.z < initPositionZ - GetComponent<Renderer>().bounds.size.z * 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
